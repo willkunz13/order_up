271d69d503a084eadf931d2ff5b43009e14934ce
jerry <jerry@jerrys-MacBook-Pro.local>
2020-02-21 10:52:57 -0700

    Fix to active records

--- app/controllers/chef_ingredients_controller.rb
+++ app/controllers/chef_ingredients_controller.rb
@@ -1,11 +1,18 @@
 class ChefIngredientsController < ApplicationController
 	def index
 		chef = Chef.find(params[:chef_id])
-		dishes = chef.dishes
-		ingredients = []
-		dishes.each do |dish|
-			ingredients << dish.ingredients
-		end
-		@ingredients = ingredients.uniq.flatten(1)
+		@ingredients = Chef.select(Ingredient.arel_table[:name]).where(Chef.arel_table[:name].eq(chef.name)).joins(
+  Chef.arel_table.join(Dish.arel_table).on(
+    Chef.arel_table[:id].eq(Dish.arel_table[:chef_id])
+  ).join_sources
+).joins(
+  Chef.arel_table.join(DishIngredient.arel_table).on(
+    Dish.arel_table[:id].eq(DishIngredient.arel_table[:dish_id])
+  ).join_sources
+).joins(
+  Chef.arel_table.join(Ingredient.arel_table).on(
+    DishIngredient.arel_table[:ingredient_id].eq(Ingredient.arel_table[:id])
+  ).join_sources
+).uniq
 	end
 end
