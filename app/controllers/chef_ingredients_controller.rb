77776f1d144727d635818ef0419b1fa568b87c70
jerry <jerry@jerrys-MacBook-Pro.local>
2020-02-21 10:58:48 -0700

    Update to check by chef id, not name

--- app/controllers/chef_ingredients_controller.rb
+++ app/controllers/chef_ingredients_controller.rb
@@ -1,7 +1,7 @@
 class ChefIngredientsController < ApplicationController
 	def index
 		chef = Chef.find(params[:chef_id])
-		@ingredients = Chef.select(Ingredient.arel_table[:name]).where(Chef.arel_table[:name].eq(chef.name)).joins(
+		@ingredients = Chef.select(Ingredient.arel_table[:name]).where(Chef.arel_table[:id].eq(chef.id)).joins(
   Chef.arel_table.join(Dish.arel_table).on(
     Chef.arel_table[:id].eq(Dish.arel_table[:chef_id])
   ).join_sources
@@ -14,5 +14,5 @@ class ChefIngredientsController < ApplicationController
     DishIngredient.arel_table[:ingredient_id].eq(Ingredient.arel_table[:id])
   ).join_sources
 ).uniq
-	end
+end
 end
